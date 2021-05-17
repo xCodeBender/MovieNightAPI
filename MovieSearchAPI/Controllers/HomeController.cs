75f63539d0f8b7c64395846a499a58d61fbd1185
Alice Ogadinma <aliceamarawell@gmail.com>
2021-05-16 23:50:00 -0700

    first commit

---
+++ MovieSearchAPI/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieSearchAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieSearchAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieDAL movieDAL = new MovieDAL();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieSearch(string Title = "")
+        {
+            MovieModel movieModel = new MovieModel();
+            if (Title != "")
+            {
+                movieModel = movieDAL.ConvertJSONtoSingleTitleMovieModel(Title);
+                return View(movieModel);
+            }
+            else
+            {
+                return RedirectToAction("MovieSearch");
+            }
+        }
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieNight(string Title1, string Title2, string Title3)
+        {
+            if (string.IsNullOrEmpty(Title1) && string.IsNullOrEmpty(Title2) && string.IsNullOrEmpty(Title3))
+            {
+                return View();
+            }
+            List<string> movieList = new List<string>
+            {
+                new string(Title1),
+                new string(Title2),
+                new string(Title3)
+            };
+            List<MovieModel> movieResult = new List<MovieModel>();
+
+            if (movieList != null)
+            {
+                foreach (string movie in movieList)
+                {
+                    var movieDetails = movieDAL.ConvertJSONtoSingleTitleMovieModel(movie);
+                    movieResult.Add(movieDetails);
+                }
+
+                return View(movieResult);
+            }
+            else
+            {
+                return RedirectToAction("MovieNight");
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieSearchAPI/Models/MovieDAL.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MovieSearchAPI.Models
+{
+    public class MovieDAL
+    {
+        public string GetMovieTitleFromJson(string title)
+        {
+            string key = "eff05e7d";
+            string url = $"http://www.omdbapi.com/?apikey={key}&t={title}";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+            return JSON;
+        }
+
+        public MovieModel ConvertJSONtoSingleTitleMovieModel(string Title)
+        {
+            string rawJSON = GetMovieTitleFromJson(Title);
+            MovieModel movie = JsonConvert.DeserializeObject<MovieModel>(rawJSON);
+            return movie;
+        }
+    }
+}
